c7d889f703a0139e6af9ecedf30de8f3815c2f9b
kerimbr <dev.kerimbora@gmail.com>
2023-01-02 17:31:38 +0300

    API_KEY hide settings done

---
+++ app/src/main/java/com/kerimbr/kotlinsuperleague/core/AppConstants.kt
@@ -0,0 +1,6 @@
+package com.kerimbr.kotlinsuperleague.core
+
+import com.kerimbr.kotlinsuperleague.BuildConfig
+
+const val API_HOST = "v3.football.api-sports.io"
+const val API_KEY = BuildConfig.API_KEY
